e056392f36757f279af71410c70027ad3beb71e6
Fedor <lfa1@yandex.ru>
2022-02-10 17:59:37 +0300

    edit html

--- storage/html/scripts.js
+++ storage/html/scripts.js
@@ -31,76 +31,70 @@ function TogglePassMqtt() {
   }
 }
 
-function setpath(elem) {
-    var default_path = elem.files[0].name;
+function setFileName(elem) {
+    var fileName = elem.files[0].name;
     if (elem.id == "newhtmlfile") {
-        document.getElementById("uploadhtml").value = default_path;
+        document.getElementById("uploadhtml").value = fileName;
     } else if (elem.id == "newcertfile") {
-        document.getElementById("uploadcert").value = default_path;
+        document.getElementById("uploadcert").value = fileName;
     } else if (elem.id == "newbinfile") {
-        document.getElementById("uploadbin").value = default_path;
+        document.getElementById("uploadbin").value = fileName;
     }
 }
-function upload(elem) {
-    var filePath = elem.value;
-    var upload_path = "";
-    var fileInput = "";
+
+async function upload(elem) {
+    var fileName = elem.value;
+    var upload_path;
+    var fileInput;
+    var element;
     if (elem.id == "uploadhtml") {
-        upload_path = "/upload/html/" + filePath;
-        fileInput = document.getElementById("newhtmlfile").files;
+        upload_path = "/upload/html/" + fileName;
+        element = document.getElementById("newhtmlfile");
     } else if (elem.id == "uploadcert") {
-        upload_path = "/upload/certs/" + filePath;
-        fileInput = document.getElementById("newcertfile").files;
+        upload_path = "/upload/certs/" + fileName;
+        element = document.getElementById("newcertfile");
     } else if (elem.id == "uploadbin") {
-        upload_path = "/upload/image/" + filePath;
-        fileInput = document.getElementById("newbinfile").files;
+        upload_path = "/upload/image/" + fileName;
+        element = document.getElementById("newbinfile");
     }
 
-    /* Max size of an individual file. Make sure this
-     * value is same as that set in file_server.c */
-    var MAX_FILE_SIZE = 200*1024;
-    var MAX_FILE_SIZE_STR = "200KB";
+    fileInput = element.files;
 
     if (fileInput.length == 0) {
         alert("No file selected!");
-    } else if (filePath.length == 0) {
-        alert("File path on server is not set!");
-    } else if (filePath.indexOf(' ') >= 0) {
-        alert("File path on server cannot have spaces!");
-    } else if (filePath[filePath.length-1] == '/') {
-        alert("File name not specified after path!");
-    } else if (fileInput[0].size > 200*1024 && elem.id != "uploadbin") {
-        alert("File size must be less than 200KB!");
     } else {
-        elem.disabled = true;
-        if (elem.id == "uploadhtml") {
-            document.getElementById("newhtmlfile").disabled = true;
-        } else if (elem.id == "uploadcert") {
-            document.getElementById("newcertfile").disabled = true;
-        } else if (elem.id == "uploadbin") {
-            document.getElementById("newbinfile").disabled = true;
-        }
-
         var file = fileInput[0];
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (xhttp.readyState == 4) {
-                if (xhttp.status == 200) {
-                    document.open();
-                    document.write(xhttp.responseText);
-                    document.close();
-                } else if (xhttp.status == 0) {
-                    alert("Server closed the connection abruptly!");
-                    location.reload()
-                } else {
-                    alert(xhttp.status + " Error!\n" + xhttp.responseText);
-                    location.reload()
-                }
+
+        document.getElementById("uploading").innerHTML = "Uploading! Please wait.";
+        
+        document.getElementById("uploadbin").disabled = true;
+        document.getElementById("newcertfile").disabled = true;
+        document.getElementById("uploadhtml").disabled = true;
+        
+        try {
+            var response = await fetch(upload_path, {
+                method: 'POST',
+                body: file
+            });
+            if (response.ok) {
+                var data = await response.text();
+                alert(data);
+            } else {
+                var error = await response.text();
+                var message = `${error}. HTTP error ${response.status}.`;
+                alert(message);
             }
-        };
-        xhttp.open("POST", upload_path, true);
-        xhttp.send(file);
+            
+        }
+        catch(error) {
+            alert(`Error! ${error}`);
+        }
+        
+        document.getElementById("newbinfile").value = "";
+        document.getElementById("newcertfile").value = "";
+        document.getElementById("newhtmlfile").value = "";
+
+        location.reload();
     }
 }
-
 
